773df26c8cb5e18e9d98dd3418eecf99a6f65e57
Salomão <snlimadev@gmail.com>
2024-08-10 16:46:49 -0300

    Updating target sdk

--- app/src/main/java/com/snlimadev/basicsocialmedia/LauncherActivity.java
+++ app/src/main/java/com/snlimadev/basicsocialmedia/LauncherActivity.java
@@ -1,54 +1,15 @@
-/*
- * Copyright 2020 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.snlimadev.basicsocialmedia;
-
-import android.content.pm.ActivityInfo;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
-
-
-
-public class LauncherActivity
-        extends com.google.androidbrowserhelper.trusted.LauncherActivity {
-    
-
-    
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Setting an orientation crashes the app due to the transparent background on Android 8.0
-        // Oreo and below. We only set the orientation on Oreo and above. This only affects the
-        // splash screen and Chrome will still respect the orientation.
-        // See https://github.com/GoogleChromeLabs/bubblewrap/issues/496 for details.
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
-        } else {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
-        }
-    }
-
-    @Override
-    protected Uri getLaunchingUrl() {
-        // Get the original launch Url.
-        Uri uri = super.getLaunchingUrl();
-
-        
-
-        return uri;
-    }
-}
+#1(D)[2024-08-10 19:16:54,152]=16:16:54=*00000*{3956}&5387DB90&<LoggerUtil>       Logger initialized (level 1, privacy mode 1, PID 3952)
+#3(I)[2024-08-10 19:16:54,183]=16:16:54=*00000*{3956}&444A5144&<EventReporter>    Initializing EventReporter with observer(SSAS, MdnsPlugin)
+#4(I)[2024-08-10 19:16:54,183]=16:16:54=*00000*{3956}&7D326D03&<LogUploader>      Initializing LogUploader with observer(SSAS, MdnsPlugin)
+#5(I)[2024-08-10 19:16:54,183]=16:16:54=*00000*{3956}&00000000&<MbPluginTaskQueu> MdnsPlugin: Initialize worker thread for MdnsPlugin
+#6(I)[2024-08-10 19:16:54,196]=16:16:54=*00000*{5128}&00000000&<MbPluginTaskQueu> MdnsPlugin: Worker thread started for MdnsPlugin
+#7(D)[2024-08-10 19:16:54,197]=16:16:54=*00000*{5128}&00000000&<MbPluginTaskQueu> MdnsPlugin: Start initializing the MdnsPlugin
+#8(I)[2024-08-10 19:16:54,200]=16:16:54=*00017*{3956}&3B07DCB2&<GfeWebService>    SSAU mode is: 0
+#9(I)[2024-08-10 19:16:54,200]=16:16:54=*00000*{3956}&FF1200F2&<GfeWebService>    Snoopy check is 0
+#0(I)[2024-08-10 19:16:54,204]=16:16:54=*00003*{3956}&3835346A&<GfeWebService>    Running on co-proc system
+#1(I)[2024-08-10 19:16:54,204]=16:16:54=*00000*{3956}&ED182843&<NvStreamControl>  NvStreamControl: PID 3952 OpenSSL 1.1.1q  5 Jul 2022
+#2(I)[2024-08-10 19:16:54,204]=16:16:54=*00000*{3956}&8ED452E2&<VersionDump>      Build Information:
+#3(M)[2024-08-10 19:16:54,204]=16:16:54=*00000*{3956}&1768021A&<VersionDump>      Name: unnamed
+#4(M)[2024-08-10 19:16:54,204]=16:16:54=*00000*{3956}&CF6CAAC5&<VersionDump>      DVS Branch: gcomp/rel/gs_04_50
+#5(M)[2024-08-10 19:16:54,204]=16:16:54=*00000*{3956}&EA649458&<VersionDump>      DVS P4VirtualChangelist: 33752090
+#6(D)[2024-08-10 19:16:54,205]=16:16:54=*00001*{3956}&D2A43FCF&<ServiceUtil>      
